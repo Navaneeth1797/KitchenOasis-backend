d081a168040604ec15f2191496f5de04680ed46a
Navaneeth1797 <rnavaneethk1797@gmail.com>
2024-03-24 15:07:40 +0530

    last commit

--- index.js
+++ index.js
@@ -1,46 +1,30 @@
-
 import express from "express";
 import cors from "cors";
-let app = express();
-
 import dotenv from "dotenv";
-
 import cookieParser from "cookie-parser";
 import { connectDatabase } from "./config/dbConnect.js";
 import errorMiddleware from "./middlewares/errors.js";
 
-
 dotenv.config();
 
-// handle uncaught exceptions
+const app = express();
 
-process.on("uncaughtException", (err) => {
-    console.log(`Error: ${err}`);
-    console.log("server rejected");
-    server.close(() => {
-        process.exit(1);
-    })
-})
-
-
-
-
-//connecting to database
-connectDatabase()
-
-
-// Middleware to parse incoming JSON requests
-app.use(express.json({limit: '10mb', verify:(req, res, buf)=>{
-    req.rawBody = buf.toString()
-}}));
+// Middleware
+app.use(
+  express.json({
+    limit: "10mb",
+    verify: (req, res, buf) => {
+      req.rawBody = buf.toString();
+    },
+  })
+);
 app.use(cookieParser());
-
-
-// Allow requests from specified origin
 app.use(cors({ origin: "https://kitchenoasis-1.onrender.com/" }));
 
+// Connect to MongoDB Atlas
+connectDatabase();
 
-//import all routes 
+// Routes
 import productRoutes from "./routes/product.js";
 import authRoutes from "./routes/auth.js";
 import orderRoutes from "./routes/order.js";
@@ -48,24 +32,29 @@ import paymentRoutes from "./routes/payment.js";
 
 app.use("/api", productRoutes);
 app.use("/api", authRoutes);
-//app.use("/api", orderRoutes);
 app.use("/api", orderRoutes);
 app.use("/api", paymentRoutes);
 
+// Error handling middleware
 app.use(errorMiddleware);
 
-let server = app.listen(process.env.PORT, () => {
-  console.log(
-    `server started on port:${process.env.PORT} in ${process.env.NODE_ENV} mode.`
-  );
+const PORT = process.env.PORT || 8001;
+
+// Start the server
+const server = app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
 
-// Unhandled promise rejection
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
 
-process.on("unhandledRejection", (err) => {
-    console.log(`Error: ${err}`);
-    console.log("server rejected");
-    server.close(() => {
-        process.exit(1);
-    })
-})
+// Handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
