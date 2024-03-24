69427b9bd0699a5bd8c9573ea1bb071ff033a663
Navaneeth1797 <rnavaneethk1797@gmail.com>
2024-03-24 12:35:28 +0530

    last commit

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDatabase = () => {
   let DB_URI =
     process.env.DB_URI ||
-    "mongodb://rnavaneethk1797:navaneeth@ac-k4n25yo-shard-00-00.c5x1ep2.mongodb.net:27017,ac-k4n25yo-shard-00-01.c5x1ep2.mongodb.net:27017,ac-k4n25yo-shard-00-02.c5x1ep2.mongodb.net:27017/?ssl=true&replicaSet=atlas-dbl1tg-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
+    "mongodb://rnavaneethk1797:navaneeth@ac-o42eblf-shard-00-00.dnthuz8.mongodb.net:27017,ac-o42eblf-shard-00-01.dnthuz8.mongodb.net:27017,ac-o42eblf-shard-00-02.dnthuz8.mongodb.net:27017/?ssl=true&replicaSet=atlas-c7wkf5-shard-0&authSource=admin&retryWrites=true&w=majority&appName=oasis";
 
   mongoose.connect(DB_URI).then((con) => {
     console.log(
--- handle/handle.js
+++ handle/handle.js
@@ -6,7 +6,7 @@ const handleProducts = async () => {
   try {
     await mongoose.connect(
       process.env.DB_URI ||
-        "mongodb://rnavaneethk1797:navaneeth@ac-k4n25yo-shard-00-00.c5x1ep2.mongodb.net:27017,ac-k4n25yo-shard-00-01.c5x1ep2.mongodb.net:27017,ac-k4n25yo-shard-00-02.c5x1ep2.mongodb.net:27017/?ssl=true&replicaSet=atlas-dbl1tg-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0",
+        "mongodb://rnavaneethk1797:navaneeth@ac-o42eblf-shard-00-00.dnthuz8.mongodb.net:27017,ac-o42eblf-shard-00-01.dnthuz8.mongodb.net:27017,ac-o42eblf-shard-00-02.dnthuz8.mongodb.net:27017/?ssl=true&replicaSet=atlas-c7wkf5-shard-0&authSource=admin&retryWrites=true&w=majority&appName=oasis",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
